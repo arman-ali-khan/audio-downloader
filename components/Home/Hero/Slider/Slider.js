ccdc83fca4e915aab9e192382aac7c957c656fbe
Arman Khan <sparmankhan@gmail.com>
2023-04-24 14:32:52 +0600

    slider make dynamic

--- components/Home/Hero/Slider/Slider.js
+++ components/Home/Hero/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,8 +12,20 @@ import "./styles.module.css";
 // import required modules
 import { Navigation, Pagination } from "swiper";
 import Link from "next/link";
+import axios from "axios";
 
 export default function Slider() {
+  // loading
+  const [loading,setLoading] = useState(true)
+  // files 
+  const [files,setFiles] = useState([])
+  useEffect(()=>{
+    axios.get(`http://localhost:3000/api/featured`)
+    .then(res=>{
+      setFiles(res.data)
+      setLoading(false)
+    })
+  },[])
   return (
     <>
       <Swiper
@@ -22,42 +34,29 @@ export default function Slider() {
         modules={[Navigation, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide className="md:!h-96 sm:!h-64 !h-44">
-            <div className="bg-[url('https://img.youtube.com/vi/4JqWHLZye80/maxresdefault.jpg')] w-full h-full bg-cover bg-center  flex justify-center items-center">
-            <div className="absolute h-full w-full backdrop-blur-sm backdrop-brightness-90 backdrop-hue-rotate-30 backdrop-contrast-75 flex flex-col px-4 justify-center items-center py-2 rounded-md">
-                <div>
-                <h2 className="md:text-3xl text-center text-info font-bold">Pashaner Khudha Part 1 | Gajendra Kumar Mitra | Mirchi Bangla</h2>
-                <div className="flex justify-center my-3">
-                <Link className="btn btn-xs md:btn-md md:w-44 btn-primary" href={'#'}>Download</Link>
-                </div>
-                </div>
+        {
+          loading ?  <div className="md:!h-96 my-3 sm:!h-64 !h-44 bg-gray-400 animate-pulse flex items-center justify-center w-full">
+            <div className="w-full mx-12 space-y-4">
+            <div className="bg-gray-300 animate-pulse h-6 w-full rounded-full "></div>
+            <div className="bg-gray-300 animate-pulse h-6 w-96 mx-auto rounded-full "></div>
             </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide className="md:!h-96 sm:!h-64 !h-44">
-            <div className="bg-[url('https://img.youtube.com/vi/4JqWHLZye80/maxresdefault.jpg')] w-full h-full bg-cover bg-center  flex justify-center items-center">
-            <div className="absolute h-full w-full backdrop-blur-sm backdrop-brightness-90 backdrop-hue-rotate-30 backdrop-contrast-75 flex flex-col px-4 justify-center items-center py-2 rounded-md">
-                <div>
-                <h2 className="md:text-3xl text-center text-info font-bold">Pashaner Khudha Part 1 | Gajendra Kumar Mitra | Mirchi Bangla</h2>
-                <div className="flex justify-center my-3">
-                <Link className="btn btn-xs md:btn-md md:w-44 btn-primary" href={'#'}>Download</Link>
-                </div>
-                </div>
-            </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide className="md:!h-96 sm:!h-64 !h-44">
-            <div className="bg-[url('https://img.youtube.com/vi/4JqWHLZye80/maxresdefault.jpg')] w-full h-full bg-cover bg-center  flex justify-center items-center">
-            <div className="absolute h-full w-full backdrop-blur-sm backdrop-brightness-90 backdrop-hue-rotate-30 backdrop-contrast-75 flex flex-col px-4 justify-center items-center py-2 rounded-md">
-                <div>
-                <h2 className="md:text-3xl text-center text-info font-bold">Pashaner Khudha Part 1 | Gajendra Kumar Mitra | Mirchi Bangla</h2>
-                <div className="flex justify-center my-3">
-                <Link className="btn btn-xs md:btn-md md:w-44 btn-primary" href={'#'}>Download</Link>
-                </div>
-                </div>
-            </div>
-            </div>
-        </SwiperSlide>
+          </div>
+          :
+            files?.map(file=> <SwiperSlide key={file._id} className="md:!h-96 my-3 sm:!h-64 !h-44">
+          <div style={{backgroundImage:`url(${file.thumb})`}} className={`w-full h-full bg-cover bg-center  flex justify-center items-center`}>
+          <div className="absolute h-full w-full backdrop-blur-sm backdrop-brightness-90 backdrop-hue-rotate-30 backdrop-contrast-75 flex flex-col px-4 justify-center items-center py-2 rounded-md">
+              <div>
+              <h2 className="md:text-3xl text-center text-info font-bold">Pashaner Khudha Part 1 | Gajendra Kumar Mitra | Mirchi Bangla</h2>
+              <div className="flex justify-center my-3">
+              <Link className="btn btn-xs md:btn-md md:w-44 btn-primary" href={'#'}>Download</Link>
+              </div>
+              </div>
+          </div>
+          </div>
+      </SwiperSlide>)
+        
+        }
+       
       </Swiper>
     </>
   );
---
+++ pages/api/featured/index.js
@@ -0,0 +1,9 @@
+import { connectToDatabase } from "../../../utils/db";
+
+
+export default async function handler(req, res) {
+  const { db } = await connectToDatabase();
+  const posts = await db.collection("files").find().sort({title:1}).limit(5).toArray();
+
+  res.status(200).send(posts);
+}
